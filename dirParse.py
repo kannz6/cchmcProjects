b8be2e1b44698f909e2f073c3346cfc22c85643e
bryan <bryan.kanu@cchmc.org>
2017-01-26 09:11:59 -0500

    refactor backup

--- dirParse.py
+++ dirParse.py
@@ -23,7 +23,24 @@ def get_filepaths( directory ):
 # these two containers are duplicatedon lines 37 and 39 for scope. At this level, scope is all files for uniqueness
 # at the next level, scope is local to the file being parsed
 #container to store all the relation strings and FID's if needed
-kin_file_full_relation_list = []; kin_file_fid_value_list = [];
+# kin_file_full_relation_list = [];  kin_file_fid_value_list = []; 
+#  using these variables from refactor
+kin_file_full_relation_list2 = []; kin_file_fid_value_list2 = [];
+
+def appendFIDValueList( v ):
+	if ( kin_file_fid_value_list2.count( v ) == 0 ):
+		kin_file_fid_value_list2.append( v )
+
+def appendRelationStringList( ro, rr ):
+	if ( kin_file_full_relation_list2.count( ro ) == 0 and kin_file_full_relation_list2.count( rr ) == 0 ):
+		kin_file_full_relation_list2.append( ro )
+
+def createRelationStrings( variablesList ): 
+	relation_str = variablesList[0] + " " + variablesList[2] + " " + variablesList[7]
+	reverse_relation_str = variablesList[2] + " " + variablesList[0] + " " + variablesList[7]
+	appendFIDValueList( variablesList[0] )
+	appendFIDValueList( variablesList[2] )
+	appendRelationStringList( relation_str, reverse_relation_str )
 
 # Run the above function and store its results in a variable.   
 full_file_paths = get_filepaths( "../../sandbox" )
@@ -43,23 +60,26 @@ for found_file in full_file_paths:
 
 			#show which directory we're in
 			with open( kin_file_in_directory ) as kin_file:
+
 				for line in kin_file:
 					#find relevant data in file
 					kinFileVariables = line.split()
 					if (kinFileVariables[0] == 'FID1'):
 						continue
 
-					relation_str = kinFileVariables[0] + " " + kinFileVariables[2] + " " + kinFileVariables[7]
-					reverse_relation_str = kinFileVariables[2] + " " + kinFileVariables[0] + " " + kinFileVariables[7]
-					#check if the FID's already exist in the list, if not...append them.
-					if ( kin_file_fid_value_list.count( kinFileVariables[0] ) == 0 ):
-						kin_file_fid_value_list.append( kinFileVariables[0] )
-					if ( kin_file_fid_value_list.count( kinFileVariables[2] ) == 0 ):
-						kin_file_fid_value_list.append( kinFileVariables[2] )
+					createRelationStrings( kinFileVariables )					
 
-					#check if the relation exists in the list already, if not, append it. Make sure the reverse has not already been added
-					if ( kin_file_full_relation_list.count( relation_str ) == 0 and kin_file_full_relation_list.count( reverse_relation_str ) == 0 ):
-						kin_file_full_relation_list.append( relation_str )
+					# relation_str = kinFileVariables[0] + " " + kinFileVariables[2] + " " + kinFileVariables[7]
+					# reverse_relation_str = kinFileVariables[2] + " " + kinFileVariables[0] + " " + kinFileVariables[7]
+					# #check if the FID's already exist in the list, if not...append them.
+					# if ( kin_file_fid_value_list.count( kinFileVariables[0] ) == 0 ):
+					# 	kin_file_fid_value_list.append( kinFileVariables[0] )
+					# if ( kin_file_fid_value_list.count( kinFileVariables[2] ) == 0 ):
+					# 	kin_file_fid_value_list.append( kinFileVariables[2] )
+
+					# #check if the relation exists in the list already, if not, append it. Make sure the reverse has not already been added
+					# if ( kin_file_full_relation_list.count( relation_str ) == 0 and kin_file_full_relation_list.count( reverse_relation_str ) == 0 ):
+					# 	kin_file_full_relation_list.append( relation_str )
 					# print "Original Order: " + relation_str + "\nReverse Order: " + reverse_relation_str
 	# in this scope, containers are local to the file only
 	# for relationString in kin_file_full_relation_list:
@@ -68,9 +88,16 @@ for found_file in full_file_paths:
 	# for fID in kin_file_fid_value_list:
 	# 	print fID
 # moved out for scope so that all files can be checked for uniqueness
-for relationString in kin_file_full_relation_list:
+# for relationString in kin_file_full_relation_list:
+# 	print relationString
+
+# for fID in kin_file_fid_value_list:
+# 	print fID
+
+print "printing kin file variables:\n"
+for relationString in kin_file_full_relation_list2:
 	print relationString
 
-for fID in kin_file_fid_value_list:
+for fID in kin_file_fid_value_list2:
 	print fID
 
