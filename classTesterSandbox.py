6ac1bc945fbaee7072beeaf8db59f2ed27a44a83
bryan <bryan.kanu@cchmc.org>
2017-01-27 09:05:30 -0500

    wrapped methods in a class

---
+++ classTesterSandbox.py
@@ -0,0 +1,7 @@
+from dirParse import dirParse
+
+# testing the class
+tester = dirParse("../../sandbox")
+tester.parseDirectory()
+tester.printRelationList()
+tester.printFIDValues()
--- dirParse.py
+++ dirParse.py
@@ -2,116 +2,77 @@
 import os
 import re
 
-file_paths = []  # List which will store all of the full filepaths.
+class dirParse:
 
-def joinPaths ( rt, fname ):
-	filepath = os.path.join( rt, fname )
-	file_paths.append( filepath )  # Add it to the list.
+	def __init__(self, directory):
+		self.dir = directory
+		self.file_paths = []  
+		self.kin_file_full_relation_list = []; 
+		self.kin_file_fid_value_list = [];
 
-def get_filepaths( directory ):
-	"""
-	This function will generate the file names in a directory 
-	tree by walking the tree either top-down or bottom-up. For each 
-	directory in the tree rooted at directory top (including top itself), 
-	it yields a 3-tuple (dirpath, dirnames, filenames).
-	"""
-	# file_paths = [] # List which will store all of the full filepaths.
+	def joinPaths ( self, rt, fname ):
+		filepath = os.path.join( rt, fname )
+		self.file_paths.append( filepath )  # Add it to the list.
 
-	# Walk the tree.
-	[ [joinPaths(root, x) for x in files] for root, directories, files in os.walk( directory ) ]
-	# for root, directories, files in os.walk( directory ):
-	# 	[joinPaths(root, x) for x in files]
+	def get_filepaths( self, directory ):
+		"""
+		This function will generate the file names in a directory 
+		tree by walking the tree either top-down or bottom-up. For each 
+		directory in the tree rooted at directory top (including top itself), 
+		it yields a 3-tuple (dirpath, dirnames, filenames).
+		"""
+		# Walk the tree.
+		[ [self.joinPaths(root, f) for f in files] for root, directories, files in os.walk( directory ) ]
 
-		# for filename in files:
-		# 	# Join the two strings in order to form the full filepath.
-		# 	filepath = os.path.join( root, filename )
-		# 	file_paths.append( filepath )  # Add it to the list.
+		return self.file_paths  # Self-explanatory.
 
-	return file_paths  # Self-explanatory.
+	def appendFIDValueList(self, v ):
+		if ( self.kin_file_fid_value_list.count( v ) == 0 ):
+			self.kin_file_fid_value_list.append( v )
+	# ro is the relation string in its original order, rr is with FID1 and FID2 reveresed
+	def appendRelationStringList( self, ro, rr ):
+		if ( self.kin_file_full_relation_list.count( ro ) == 0 and self.kin_file_full_relation_list.count( rr ) == 0 ):
+			self.kin_file_full_relation_list.append( ro )
 
-# these two containers are duplicatedon lines 37 and 39 for scope. At this level, scope is all files for uniqueness
-# at the next level, scope is local to the file being parsed
-#container to store all the relation strings and FID's if needed
-# kin_file_full_relation_list = [];  kin_file_fid_value_list = []; 
-#  using these variables from refactor
-kin_file_full_relation_list2 = []; kin_file_fid_value_list2 = [];
+	def createRelationStrings( self, variablesList ):
+		relation_str = variablesList[0] + " " + variablesList[2] + " " + variablesList[7]
+		reverse_relation_str = variablesList[2] + " " + variablesList[0] + " " + variablesList[7]
+		self.appendFIDValueList( variablesList[0] )
+		self.appendFIDValueList( variablesList[2] )
+		self.appendRelationStringList( relation_str, reverse_relation_str )
 
-def appendFIDValueList( v ):
-	if ( kin_file_fid_value_list2.count( v ) == 0 ):
-		kin_file_fid_value_list2.append( v )
-# ro is the relation string in its original order, rr is with FID1 and FID2 reveresed
-def appendRelationStringList( ro, rr ):
-	if ( kin_file_full_relation_list2.count( ro ) == 0 and kin_file_full_relation_list2.count( rr ) == 0 ):
-		kin_file_full_relation_list2.append( ro )
+	def parseKinFile( self, kinFile ):
+		with open( kinFile ) as kf:
+			for line in kf:
+			#find relevant data in file
+				kinFileVariables = line.split()
+				if (kinFileVariables[0] == 'FID1'):
+					continue
 
-def createRelationStrings( variablesList ):
-	relation_str = variablesList[0] + " " + variablesList[2] + " " + variablesList[7]
-	reverse_relation_str = variablesList[2] + " " + variablesList[0] + " " + variablesList[7]
-	appendFIDValueList( variablesList[0] )
-	appendFIDValueList( variablesList[2] )
-	appendRelationStringList( relation_str, reverse_relation_str )
+				self.createRelationStrings( kinFileVariables )
 
-def parseKinFile( kinFile ):
-	with open( kinFile ) as kf:
-		for line in kf:
-		#find relevant data in file
-			kinFileVariables = line.split()
-			if (kinFileVariables[0] == 'FID1'):
-				continue
+	def printRelationList( self ):
+		print "\nRelation List:\n___________________________"
+		for r in self.kin_file_full_relation_list:
+			print r
+		print "___________________________\nEnd of Relation List"
 
-			createRelationStrings( kinFileVariables )
+	def getRelationList( self ):
+		return self.kin_file_full_relation_list
 
-# Run the above function and store its results in a variable.   
-full_file_paths = get_filepaths( "../../sandbox" )
+	def printFIDValues( self ):
+		print "\nFID Values:\n___________________________\n"
+		for fid in self.kin_file_fid_value_list:
+			print fid
+		print "___________________________\nEnd of FID Values"
 
-# get all the files that match the file extension we are looking for
-list_of_kin_files = filter( (lambda x : re.match( r'(.*\/.*kin0)', x) ), full_file_paths )
-# using the list from the kin files, extract the variables we need
-map( parseKinFile, list_of_kin_files )
-#use this in the table
-# for found_file in full_file_paths:
-	#search for *.kin0 files
-	# kinFileDir_regEx = re.match( r'(.*\/.*kin0)', found_file )
-	# #container to store all the relation strings if needed
-	# # kin_file_full_relation_list = [];
-	# # #list of id's for sorting for unique values before entering into the table
-	# # kin_file_fid_value_list = [];
+	def getFIDValues( self ):
+		return self.kin_file_fid_value_list
 
-	# if kinFileDir_regEx:
-
-		# print kinFileDir_regEx.groups()
-
-		# map( parseKinFile, kinFileDir_regEx.groups() )
-
-		# for kin_file_in_directory in kinFileDir_regEx.groups():
-
-		# 	with open( kin_file_in_directory ) as kin_file:
-
-		# 		for line in kin_file:
-		# 			#find relevant data in file
-		# 			kinFileVariables = line.split()
-		# 			if (kinFileVariables[0] == 'FID1'):
-		# 				continue
-
-		# 			createRelationStrings( kinFileVariables )					
-
-	# in this scope, containers are local to the file only
-	# for relationString in kin_file_full_relation_list:
-	# 	print relationString
-
-	# for fID in kin_file_fid_value_list:
-	# 	print fID
-# moved out for scope so that all files can be checked for uniqueness
-# for relationString in kin_file_full_relation_list:
-# 	print relationString
-
-# for fID in kin_file_fid_value_list:
-# 	print fID
-
-print "printing kin file variables:\n"
-for relationString in kin_file_full_relation_list2:
-	print relationString
-
-for fID in kin_file_fid_value_list2:
-	print fID
-
+	def parseDirectory( self ):
+		# Run the above function and store its results in a variable.   
+		full_file_paths = self.get_filepaths( self.dir )
+		# get all the files that match the file extension we are looking for
+		list_of_kin_files = filter( (lambda x : re.match( r'(.*\/.*kin0)', x) ), full_file_paths )
+		# using the list from the kin files, extract the variables we need
+		map( self.parseKinFile, list_of_kin_files )
